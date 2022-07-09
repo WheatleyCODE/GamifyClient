7dd8bce011ee9503565ded538462a06cf1245df9
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2022-07-09 16:09:00 +0300

    Init Project

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'import',
+  ],
+  extends: [
+    'google',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'new-cap': 'off',
+  },
+  settings: {
+    'jsx-a11y': {
+      components: {
+        MyButton: 'button',
+        RoundButton: 'button',
+      },
+    },
+  },
+};
